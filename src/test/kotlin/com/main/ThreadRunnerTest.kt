7ddcfe0b5186055ddf63ddc6575738e03e71b02e
Alex Voytovich <darking271@gmail.com>
2018-11-04 15:51:00 -0500

    Test failure fixed

--- src/test/kotlin/com/main/ThreadRunnerTest.kt
+++ src/test/kotlin/com/main/ThreadRunnerTest.kt
@@ -1,21 +1,5 @@
 package com.main
 
-import org.jboss.arquillian.container.test.api.Deployment
-import org.jboss.arquillian.junit.Arquillian
-import org.jboss.shrinkwrap.api.ShrinkWrap
-import org.jboss.shrinkwrap.api.asset.EmptyAsset
-import org.jboss.shrinkwrap.api.spec.JavaArchive
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
-@RunWith(Arquillian::class)
 object ThreadRunnerTest {
-  @Deployment
-  fun createDeployment(): JavaArchive {
-    return ShrinkWrap.create(JavaArchive::class.java)
-        .addClass(ThreadRunner::class.java)
-        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-  }
 
 }
